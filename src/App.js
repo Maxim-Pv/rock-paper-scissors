df7f008715c5b8591f0e02ffe5acfa4ab97f2012
Maksim Pavlov <maxim.paw2010@yandex.ru>
2024-07-06 22:26:44 +0300

    header develop

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import './styles.css';
 
 function App() {
   return (
-    <div>
-      <div>
-        <h1>Rock</h1>
-        <h1>Paper</h1>
-        <h1>Scissors</h1>
+    <div className="container">
+      <div className='header'>
+        <div className='title'>
+          <h1>Rock</h1>
+          <h1>Paper</h1>
+          <h1>Scissors</h1>
+        </div>
+        <div className='score'>
+          <span>score</span>
+        </div>
       </div>
-
-      <div>
-        <span>score</span>
-      </div>
+      
     </div>
   );
 }
